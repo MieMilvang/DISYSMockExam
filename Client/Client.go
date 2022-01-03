a7cf4c4fe900de1c93a88a7703be517449d9f378
miemilvang <milv@itu.dk>
2022-01-03 13:34:05 +0100

    initial commit and set up

---
+++ Client/Client.go
@@ -0,0 +1,83 @@
+
+package main
+
+import (
+	"context"
+	"fmt"
+
+	h "github.com/MieMilvang/DISYSMockExam/HelperMethod"
+	"github.com/MieMilvang/DISYSMockExam/Proto"
+	"google.golang.org/grpc"
+)
+
+type User struct {
+	userId int64
+}
+
+func main() {
+	//init
+	//setup a connection, this is a blocking call
+	conn, err := grpc.Dial(h.FRONT_END_ADDRESS, grpc.WithInsecure(), grpc.WithBlock())
+	h.CheckError(err, "Main when to FRONT_END_ADDRESS")
+	defer conn.Close()
+
+	//create client
+	ctx := context.Background()
+	client := Proto.NewProtoServiceClient(conn)
+
+	//create user
+	id := ChooseUserId()
+	user := User{userId: int64(id)}
+
+	//wait for input
+	user.ListenForInput(client, ctx)
+	fmt.Println("main has ended")
+}
+
+// listen for user input as a string and send request accordingly, implement logic in seperate methods as see fit
+func (u *User) ListenForInput(client Proto.ProtoServiceClient, ctx context.Context) {
+	for {
+		var input string
+		fmt.Scanln(&input)
+		if len(input) > 0 {
+			switch input {
+			case "joinchat":
+				response, err := client.JoinService(ctx, &Proto.JoinRequest{UserId: u.userId})
+				h.CheckError(err, "listenForInput joinchat")
+				fmt.Println(response.GetMsg())
+			case "getvalue":
+				response, err := client.GetValue(ctx, &Proto.GetRequest{})
+				h.CheckError(err, "ListenForInput getvalue")
+				fmt.Println(response)
+			case "setvalue":
+				fmt.Println("Choose a none 0, integer you want to set the value")
+				var value int64
+				fmt.Scanln(&value)
+				if value == 0 {
+					fmt.Println("try a none 0 integer")
+					break
+				}
+				response, err := client.SetValue(ctx, &Proto.SetRequest{UserId: u.userId, RequestedValue: value})
+				h.CheckError(err, "ListenForInput setvalue")
+				fmt.Println(response.GetMsg())
+			default:
+				fmt.Printf("unrecognised command: %v\n", input)
+				fmt.Println("try, 'setvalue', 'getvalue', or 'joinchat'")
+			}
+		}
+	}
+}
+
+// helper method wait for user to input a wanted userid
+func ChooseUserId() int {
+	for {
+		var userId int
+		fmt.Println("Choose a positive integer as id:")
+		fmt.Scanln(&userId)
+		if userId > 0 {
+			return userId
+		} else {
+			fmt.Printf("\nSomething went wrong when processing your input: %v\nTry a positive integer like 1, 2, 3, ...", userId)
+		}
+	}
+}
---
+++ HelperMethod/HelperMethod.go
@@ -0,0 +1,54 @@
+package HelperMethod
+
+import (
+	"context"
+	"log"
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/MieMilvang/DISYSMockExam/Proto"
+	"google.golang.org/grpc"
+)
+
+const (
+	FRONT_END_ADDRESS = "localhost:5000"
+	MAX_REPLICAS      = 5
+)
+
+type Value struct {
+	Value  int64
+	UserId int64
+}
+
+// helper method to help find error locations
+func CheckError(err error, msg string) {
+	if err != nil {
+		log.Fatalf("happened inside method: %s err: %v", msg, err)
+	}
+}
+
+// log message in file
+func Logger(message string, logFileName string) {
+	file, err := os.OpenFile(logFileName, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatalf("error opening file: %v", err)
+	}
+	defer file.Close()
+
+	log.SetOutput(file)
+	log.Println(message)
+}
+
+// connect to a port and check if alive
+func ConnectToPort(port int) (Proto.ProtoServiceClient, string) {
+	conn, err := grpc.Dial("localhost:"+strconv.Itoa(port), grpc.WithTimeout(time.Millisecond*250), grpc.WithInsecure()) // grpc.WithBlock(),
+	if err == nil {
+		ctx := context.Background()
+		defer ctx.Done()
+		client := Proto.NewProtoServiceClient(conn)
+		response, _ := client.JoinService(ctx, &Proto.JoinRequest{UserId: -1})
+		return client, response.GetMsg()
+	}
+	return nil, "unknown"
+}
